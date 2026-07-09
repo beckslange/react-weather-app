5fc9e5ae08f98608330c9925ede9e97ac193c443
beckslange <beckslange96@gmail.com>
2026-07-09 12:31:03 -0600

    added github repo url

--- src/App.js
+++ src/App.js
@@ -5,6 +5,22 @@ export default function App() {
     <div className="App">
       <header className="App-header">
         <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/rebecca-lange-804b8b111/"
+            target="_blank"
+          >
+            Rebecca Lange
+          </a>{" "}
+          and is open sourced on{" "}
+          <a
+            href="https://github.com/beckslange/react-weather-app"
+            target="_blank"
+          >
+            Github
+          </a>
+        </footer>
       </header>
     </div>
   );
