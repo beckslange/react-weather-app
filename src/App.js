b25d4638988cf55d4a718f7566b7027ab7804114
beckslange <beckslange96@gmail.com>
2026-07-09 20:05:29 -0600

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <Weather />
         <footer>
           This project was coded by{" "}
           <a
---
+++ src/Weather.js
@@ -0,0 +1,52 @@
+import React from "react";
+import "./Weather.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn-primary w-100" />
+          </div>
+        </div>
+      </form>
+      <h1>New York</h1>
+      <ul>
+        <li>Wednesday 07:00</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="d-flex align-items-center">
+            <img
+              src="https://www.gstatic.com/weather/conditions/v1/svg/partly_cloudy_light.svg"
+              alt="Mostly Cloudy"
+              className="weather-icon me-2"
+            />
+            <div className="temperature-container">
+              <span className="temperature">6</span>
+              <span className="unit ms-1">°C</span>
+            </div>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 15%</li>
+            <li>Humidity: 72%</li>
+            <li>Wind: 13 km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
