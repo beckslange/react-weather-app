c3aab6e59b114e6bc6e9cea5869182545e909ba6
beckslange <beckslange96@gmail.com>
2026-07-10 16:00:00 -0600

    added javascript functions

--- src/Weather.js
+++ src/Weather.js
@@ -1,18 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
 
-export default function Weather() {
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState(null);
+  const [city, setCity] = useState(props.defaultCity);
+
+  function formatDate(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let day = days[date.getDay()];
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+
+    if (minutes < 10) {
+      minutes = `0$(minutes)`;
+    }
+    let ampm = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+
+    return `${day} ${hours}:${minutes} ${ampm}`;
+  }
+  function refreshWeather(response) {
+    setWeatherData({
+      city: response.data.city,
+      time: formatDate(response.data.time),
+      description: response.data.condition.description,
+      humidity: response.data.temperature.humidity,
+      wind: Math.round(response.data.wind.speed),
+      temperature: Math.round(response.data.temperature.current),
+      iconURL: response.data.condition.icon_url,
+    });
+  }
+  function searchCity(city) {
+    let apiKey = "tfo33b89af42954f2d60430a801e1b3c";
+    let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+    axios.get(apiURL).then(refreshWeather);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    searchCity(city);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+    if (weatherData === null) {
+      searchCity(props.defaultCity);
+
+      return <p>Loading...</p>;
+    }
+  }
   return (
     <div className="Weather">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="row">
           <div className="col-9">
             <input
               type="search"
               placeholder="Enter a city..."
+              required
               className="form-control"
               autoFocus="on"
+              onChange={updateCity}
             />
           </div>
           <div className="col-3">
@@ -20,7 +80,7 @@ export default function Weather() {
           </div>
         </div>
       </form>
-      <h1>New York</h1>
+      <h1 className="city-name">{weatherData.city}</h1>
       <ul className="list-unstyled">
         <li>Wednesday 07:00</li>
         <li>Mostly Cloudy</li>
