fb4bd8cbea2ff7ff5cc19c2979d710426facab42
beckslange <beckslange96@gmail.com>
2026-07-10 17:34:15 -0600

    added and edited .footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Weather from "./Weather";
+import "./Weather.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         <Weather defaultCity="New York" />
-        <footer className="text-center">
+        <footer className="text-center footer">
           This project was coded by{" "}
           <a
             href="https://www.linkedin.com/in/rebecca-lange-804b8b111/"
